06cbc3d1842a50951d1dfb7a7c8f3af07c5fb8cb
laiyurong <shinnosuke0875@gmail.com>
2017-04-24 01:33:46 +0800

    <Data>convert objective-C  to swift

---
+++ Source/Classes/Translator.swift
@@ -0,0 +1,17 @@
+//
+//  Translator.swift
+//  Mumble
+//
+//  Created by 賴昱榮 on 2017/4/24.
+//
+//
+
+import Foundation
+import UIKit
+import Realm
+class Translator: NSObject {
+    var userName: String = ""
+    var rating: NSNumber?
+    var userIcon: String = ""
+    var price: NSNumber?
+}
