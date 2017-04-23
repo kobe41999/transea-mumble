5bd12afa301bd4326601e4c60c928792495219c6
kobe <linzijie@linzijiede-MacBook-Pro.local>
2017-04-23 18:45:12 +0800

    可執行version

--- Source/Classes/Mumble-Bridging-Header.h
+++ Source/Classes/Mumble-Bridging-Header.h
@@ -3,3 +3,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <STPopup/STPopup.h>
+#import "AppDelegate.h"
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <ParseFacebookUtilsV4/PFFacebookUtils.h>
+#import <DebugKit/DebugKit.h>
+#import <Parse/PFQuery.h>
